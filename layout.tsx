a31d0b86d9bde58a8b11ce8b231151909b1dc663
Gavril Bogdan <gavrilbogdan3@gmail.com>
2025-11-13 10:22:28 +0200

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,71 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Montserrat, Poppins } from "next/font/google";
+import "./globals.css";
+import RouteLoader from "./components/RouteLoader";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "TheFlowerShop | Florarie",
+  description: "TheFlowerShop",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Momo+Signature&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cookie&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+          integrity="sha512-xxxx..."
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${montserrat.className} ${poppins.className} antialiased min-h-screen bg-linear-to-br from-gray-800  to-black`}
+      >
+        <RouteLoader />
+
+        {children}
+      </body>
+    </html>
+  );
+}
